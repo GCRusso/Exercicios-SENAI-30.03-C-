28937209b109c6d5320005931cc112de17be453d
Gabriel Coral Russo <23655858841@info.local>
2023-03-31 10:50:10 -0300

    exercicio 6

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine($"Quantas maçãs voce deseja comprar?: ");
+int macas = int.Parse(Console.ReadLine());
+
+float acima = 0.25f;
+float abaixo = 0.30f;
+
+if (macas >= 12)
+{
+    float total = (acima * macas);
+    Console.WriteLine($"o valor da sua compra e de: {total}");
+
+
+}
+
+else if (macas < 12)
+{
+
+    float total = (abaixo * macas);
+    Console.WriteLine($"o valor da sua compra e de: {total}");
+
+}
+
+
+
+
+
+
+
+
+// 5)As    maçãs custam R$ 0,30 cada se forem compradas    menos do que uma dúzia, e R$ 0,25 
+// se forem compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, 
+// calcule e escreva o valor    total    da compra.
+
+// menos que 12 macas 30,00 cada 
+
---
+++ exercicio 6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine($"Qual a frequencia de presenca do aluno?: ");
+int frequencia = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual a nota do aluno?: ");
+int media = int.Parse(Console.ReadLine());
+
+
+if (frequencia >= 75 && media >= 7)
+{
+Console.WriteLine($"APROVADO!");
+
+}
+
+else if (frequencia >= 75 && media >= 3 && media < 7 )
+{
+Console.WriteLine($"O Aluno tem direito a uma ultima avaliacao! ");
+
+}
+
+else
+{
+Console.WriteLine($"REPROVADO!");
+
+}
